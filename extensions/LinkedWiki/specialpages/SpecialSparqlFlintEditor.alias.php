ae0b0c5bd0bba4d5f08696c5e0df5ade8613e7fb
remco <rdeboer@archixl.nl>
2015-10-21 13:29:16 +0200

    Braid: Add mirror 'extensions/LinkedWiki' at '049afde'

---
+++ extensions/LinkedWiki/specialpages/SpecialSparqlFlintEditor.alias.php
@@ -0,0 +1,12 @@
+<?php
+$specialPageAliases = array();
+
+/** English */
+$specialPageAliases['en'] = array(
+	'linkedwiki-specialsparqlflinteditor' => array('SparqlFlintEditor', 'Sparql Flint editor'),
+);
+
+/** Français */
+$specialPageAliases['fr'] = array(
+	'linkedwiki-specialsparqlflinteditor' => array( 'SparqlFlintEditor', 'Editeur Flint Sparql'),
+);
---
+++ extensions/LinkedWiki/specialpages/SpecialSparqlQuery.alias.php
@@ -0,0 +1,12 @@
+<?php
+$specialPageAliases = array();
+
+/** English */
+$specialPageAliases['en'] = array(
+	'linkedwiki-specialsparqlquery' => array('SparqlQuery', 'Sparql editor'),
+);
+
+/** Français */
+$specialPageAliases['fr'] = array(
+	'linkedwiki-specialsparqlquery' => array( 'SparqlQuery', 'Editeur Sparql'),
+);
