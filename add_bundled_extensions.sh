a6dccd0d7c1a84bd332f8fb8163d06b97354f0a3
Youri <yvdbogert@archixl.nl>
2017-01-06 11:22:46 +0100

    Updated script for skins

--- add_bundled_extensions.sh
+++ add_bundled_extensions.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 bundledextensions="WikiEditor TitleBlacklist SyntaxHighlight_GeSHi SpamBlacklist Renameuser Poem PdfHandler ParserFunctions Nuke LocalisationUpdate Interwiki InputBox ImageMap Gadgets ConfirmEdit CiteThisPage Cite"
+bundledskins="Vector MonoBook Modern CologneBlue"
 
 ## Add extension function
 add_extension() {
@@ -7,8 +8,13 @@ add_extension() {
 	braid add "https://gerrit.wikimedia.org/r/p/mediawiki/extensions/$2.git" core/$1/extensions/$2 --branch=$1
 }
 
+add_skin() {
+	echo "Added skin $2 to branch $1"
+	braid add "https://gerrit.wikimedia.org/r/p/mediawiki/skins/$2.git" core/$1/skins/$2 --branch=$1
+}
+
 ## Info message
-echo "In what branch should the bundled extensions be added?"
+echo "In what branch should the bundled extensions and skins be added?"
 echo " - Available branches:"
 for dir in $(ls core/)
 do
@@ -29,5 +35,11 @@ do
 	add_extension $b $ext
 done
 
+## Loop over the bundled skins to add them
+for skin in $bundledskins
+do
+	add_skin $b $skin
+done
+
 ## Echo success message
-echo "Added bundled extensions to $b"
+echo "Added bundled extensions and skins to branch: $b"
