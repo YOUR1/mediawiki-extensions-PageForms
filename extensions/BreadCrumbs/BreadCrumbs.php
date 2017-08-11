c1a032a1a43c91e6e4f6ffa6c2e3e991e2882195
WikiXL <wikixl@localhost.localdomain>
2017-08-11 13:00:43 +0200

    Braid: Update mirror 'extensions/BreadCrumbs' to 'dd00a1f'

--- extensions/BreadCrumbs/BreadCrumbs.php
+++ extensions/BreadCrumbs/BreadCrumbs.php
@@ -10,7 +10,7 @@
  * @package MediaWiki
  * @author Manuel Schneider <manuel.schneider@wikimedia.ch>, Tony Boyles <ABoyles@milcord.com>
  * @copyright © 2007 by Manuel Schneider, 2012 by Tony Boyles, Milcord llc
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * @license https://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
 
 if ( !defined( 'MEDIAWIKI' ) ) {
@@ -23,9 +23,14 @@ $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
 	'name' => 'BreadCrumbs',
 	'descriptionmsg' => 'breadcrumbs-desc',
-	'version' => '0.4.0',
-	'author' => array( 'Manuel Schneider', '[http://milcord.com Tony Boyles, Milcord llc]' ),
+	'version' => '0.5.0',
+	'author' => array(
+		'Manuel Schneider',
+		'Tony Boyles',
+		'...'
+	),
 	'url' => 'https://www.mediawiki.org/wiki/Extension:BreadCrumbs',
+	'license-name' => 'GPL-2.0+'
 );
 
 # Default Options:
@@ -79,7 +84,6 @@ $wgHooks['GetPreferences'][] = 'fnBreadCrumbsAddPreferences';
 
 # Register the internationalization file
 $wgMessagesDirs['Breadcrumbs'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['Breadcrumbs'] = dirname( __FILE__ ) . '/BreadCrumbs.i18n.php';
 
 # Load the file containing the hook functions:
 require_once( 'BreadCrumbsFunctions.php' );
@@ -87,7 +91,8 @@ require_once( 'BreadCrumbsFunctions.php' );
 # Resource loader
 $wgResourceModules['ext.breadCrumbs'] = array(
 	'styles' => 'BreadCrumbs.css',
-	'localBasePath' => dirname( __FILE__ ),
+	'position' => 'top',
+	'localBasePath' => __DIR__,
 	'remoteExtPath' => 'BreadCrumbs'
 );
 
